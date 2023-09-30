a628beea23e61fd83da1de1e576eea136616afc7
hieu <nmhieu16@gmail.com>
2023-09-30 18:50:42 +0700

    fix layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,8 +24,8 @@
                     <nav class="relative flex flex-row items-center justify-between">
                         <div class="absolute pl-5 text-xl font-semibold text-center cursor-pointer logo basis-2/6">
                             CoffeeStyle.
-                        </div>
-                        <!-- End logo -->
+                        </div><!-- End logo -->
+
                         <ul class="hidden text-sm font-medium text-gray-500 uppercase basis-3/6 lg:flex lg:items-center lg:justify-end lg:gap-8"
                             id="ct-top-menu">
                             <li class="ct-top-menu-item">
@@ -49,8 +49,8 @@
                             <li class="px-2 ct-top-menu-item ct-top-menu-item-active">
                                 <a class="" href="https://github.com/ueih16" target="blank">Ueih</a>
                             </li>
-                        </ul>
-                        <!-- End list products -->
+                        </ul><!-- End list products -->
+
                         <ul
                             class="flex items-center justify-end ml-16 text-sm font-medium text-gray-500 uppercase basis-3/6 lg:basis-1/6 lg:justify-start">
                             <li class="">
@@ -65,8 +65,8 @@
                                     <span class="text-white bg-orange-400 ct-badge-circle">99+</span>
                                 </a>
                             </li>
-                        </ul>
-                        <!-- End cart -->
+                        </ul><!-- End cart -->
+
                         <!-- Start list -->
                         <div class="flex items-center px-4 basis-1/6 hover:cursor-pointer sm:px-0 lg:hidden">
                             <svg class="w-6 h-6" fill="none" id="toggle-ct-top-menu" stroke-width="1.5"
@@ -75,11 +75,11 @@
                                     d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
                                     stroke-linecap="round" stroke-linejoin="round" />
                             </svg>
-                        </div>
-                        <!-- End list -->
+                        </div><!-- End list -->
+
                     </nav>
-                </header>
-                <!-- End header section -->
+                </header><!-- End header section -->
+
                 <!-- Start main section -->
                 <main>
                     <!-- Start slider -->
@@ -105,8 +105,8 @@
                                 </a>
                             </div>
                         </div>
-                    </div>
-                    <!-- End slider -->
+                    </div><!-- End slider -->
+
                     <!-- Start story -->
                     <div class="flex items-center justify-center story">
                         <div class="py-32 text-center sm:px-4 md:px-32 lg:px-52 xl:px-64">
@@ -124,20 +124,41 @@
                                 Read the full Story
                             </a>
                         </div>
-                    </div>
-                    <!-- End slider story -->
+                    </div><!-- End slider story -->
+
+
                     <!-- Start feature-mugs -->
                     <div class="ct-subheadline">
                         <div class="ct-subheadline-deco-line"></div>
                         <label class="ct-subheadline-label">Feature Mugs</label>
                         <div class="ct-subheadline-deco-line"></div>
                     </div>
-                    <div class="feature-mugs mx-auto mb-24 w-[65%]">
-                        <div class="grid">
-                            <div class="border-2 ct-product-card"></div>
+                    <div class="feature-mugs w-[65%] mb-24 mx-auto">
+                        <div class="grid grid-cols-2 gap-4">
+                            <div class="ct-product-card">
+                                <div class="border-2 mx-2 h-[540px] w-[460px] border-[#A85F4B] bg-[url('../assets/images/feature-mug-1.jpg')] bg-cover bg-no-repeat bg-center">
+                                    <a href="#" Alt="product img">
+                                        <div class="relative w-full h-full ease-in-out hover:bg-gray-900 hover:bg-opacity-10 hover:transition-all hover:duration-400 group">
+                                            <div class="absolute top-3 right-3 w-[100px] px-4 py-2 bg-white text-center font-semibold text-[#A85F4B]">On Sale.</div>
+                                            <div class="absolute hidden group-hover:block group-hover:animate-fadeIn w-11/12 px-8 py-4 text-[12px] font-semibold tracking-widest text-center text-gray-900 uppercase -translate-x-1/2 bg-white cursor-pointer left-1/2 bottom-4 hover:bg-opacity-90">EXPLORE MUGS</div>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="">
+                                    <a href="#" class="ct-product"></a>
+                                </div>
+                            </div>
+
+                            <div class="border-2 ct-product-card mx-2 h-[540px] w-[460px] border-[#A85F4B] bg-[url('../assets/images/feature-mug-2.jpg')] bg-cover bg-no-repeat bg-center">
+                                <a href="#" Alt="product img">
+                                    <div class="w-full h-full div">
+
+                                    </div>
+                                </a>
+                            </div>
                         </div>
-                    </div>
-                    <!-- End slider feature-mugs -->
+                    </div><!-- End slider feature-mugs -->
+
                     <!-- Start more-products -->
                     <div class="ct-subheadline">
                         <div class="ct-subheadline-deco-line"></div>
@@ -146,8 +167,8 @@
                     </div>
                     <div class="more-products">
                         more-products
-                    </div>
-                    <!-- End slider more-products -->
+                    </div><!-- End slider more-products -->
+
                     <!-- Start coffee-magazine -->
                     <div class="ct-subheadline">
                         <div class="ct-subheadline-deco-line"></div>
@@ -156,8 +177,8 @@
                     </div>
                     <div class="coffee-magazine">
                         coffee-magazine
-                    </div>
-                    <!-- End slider coffee-magazine -->
+                    </div><!-- End slider coffee-magazine -->
+
                     <!-- Start lifestyle-story -->
                     <div class="ct-subheadline">
                         <div class="ct-subheadline-deco-line"></div>
@@ -166,22 +187,22 @@
                     </div>
                     <div class="lifestyle-story">
                         lifestyle-story
-                    </div>
-                    <!-- End slider lifestyle-story -->
+                    </div><!-- End slider lifestyle-story -->
+
                     <!-- Start subscribe-us -->
                     <div class="subscribe-us">
                         subscribe-us
-                    </div>
-                    <!-- End slider subscribe-us -->
-                </main>
-                <!-- End main -->
+                    </div><!-- End slider subscribe-us -->
+
+                </main><!-- End main -->
+
                 <!-- Start footer section -->
                 <footer>
                     Footer
-                </footer>
-                <!-- End footer section -->
-            </div>
-            <!-- End content wrapper -->
+                </footer><!-- End footer section -->
+
+            </div><!-- End content wrapper -->
+
         </div>
     </body>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -23,10 +23,12 @@ module.exports = {
                 sildeDown: {
                     '0%': { transform: 'translateY(-100%)' },
                     '100%': { transform: 'translateY(0)' },
-                }
+                },
+                fadeIn: { from: { opacity: '0' }, to: { opacity: '1' } },
             },
             animation: {
                 sildeDown: 'sildeDown 0.5s ease-in-out',
+                fadeIn: 'fadeIn 0.5s ease-in-out',
             },
             backgroundImage: {
                 'slider-bg' : 'url("../assets/images/slide-picture.jpg")',
